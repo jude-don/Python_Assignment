366cfde7be1db5bfd597debd6fa6b719a32d83ea
SavvyPabi <67007684+SavvyPabi@users.noreply.github.com>
2020-08-01 14:23:59 +0000

    Update FundsGenerator.py

--- FundsGenerator.py
+++ FundsGenerator.py
@@ -32,8 +32,55 @@ if answer == "Y" or answer == "y": # allow room for capitalization error
     second_hour = st.hour
     second_minute = st.minute
     
-    
-    import openpyxl
+else:
+    print("Oh! you already completed the task? Great! Hit the enter key and I'll tell you how long you worked and how much you earned.") 
+    first_year = int(input("Please enter year of the start date: \n"))
+ 
+    first_month = int(input("Please enter month of the start date figures from 1-12: \n"))
+    if first_month == 2 and first_year%4 == 0:
+        first_day = int(input("Please enter day of start date, remember this year february has 29 days: \n")) #if it's February in a leap year, we'd expect an input a number between 1 and 29
+    elif first_month == 2 and first_year%4 != 0:
+        first_day = int(input("Please enter day of start date, remember february has 28 days: \n"))#if it's February in a regular year, we'd expect an input a number between 1 and 28
+    elif first_month in [9,4,6,11]:
+        first_day = int(input("Please enter day of start date. should be between 1 and 30: \n"))# September, April, June and November have 30 days
+    else:
+        first_day = int(input("Please enter day of start date. should be between 1 and 31: \n"))    
+
+    first_time = input("Please enter time of start date in 24 hour format, separated by commas. For example 12hours 30 minutes as 12,30: \n")
+    mylist = first_time.split(',')
+    first_hour = int(mylist[0])
+    first_minute = int(mylist[1])
+
+    # algorithm to obtain end date and time
+    second_year = int(input("Please enter year of the end date: \n"))
+
+    second_month = int(input("Please enter month of the end date figures from 1-12: \n"))
+
+    second_day = int(input("Please enter day of end date: \n"))
+
+    second_time = input("Please enter time of end date in 24 hour format, separated by commas. For example 12hours 30 minutes as 12,30: \n")
+    mylist1 = second_time.split(',')
+    second_hour = int(mylist1[0])
+    second_minute = int(mylist1[1])
+
+
+# Algorithm to find time difference and hours worked
+first_date = datetime(first_year,first_month,first_day,first_hour,first_minute)
+
+second_date = datetime(second_year,second_month,second_day,second_hour,second_minute)
+
+difference_between_datetimes = second_date - first_date
+hours_calculated = difference_between_datetimes.total_seconds()/3600
+total_amount_received = hours_calculated * PayRate
+currency = "$"
+print('Great work %s! You spent %f minutes, which is %f %s on the task. And you earned %s%f' % (username,hours_calculated*60,hours_calculated,'hours',currency,total_amount_received))
+print('Your bill is $200')
+time.sleep(5)
+print("Don't mind me, I'm just kidding.\nBye %s, hope to see you soon!" %username)
+
+
+# Converting to an Excel file
+ import openpyxl
 print("Please choose create or exisiting per the instructions in the next line. However, if you did not create the excel file with this program please do not choose exisitng for simplicity.")
 decision = input("Please type Create if you want to create a new excel file or type Exisitng if you want to use an existing file. \n")
 if decision == "Create" or decision == "create":
@@ -91,50 +138,3 @@ else:
     F3.value = str(hours_calculated)
     G3.value = "$" + str(total_amount_received)
     book.save(filename)
-
-else:
-    print("Oh! you already completed the task? Great! Hit the enter key and I'll tell you how long you worked and how much you earned.") 
-    first_year = int(input("Please enter year of the start date: \n"))
- 
-    first_month = int(input("Please enter month of the start date figures from 1-12: \n"))
-    if first_month == 2 and first_year%4 == 0:
-        first_day = int(input("Please enter day of start date, remember this year february has 29 days: \n")) #if it's February in a leap year, we'd expect an input a number between 1 and 29
-    elif first_month == 2 and first_year%4 != 0:
-        first_day = int(input("Please enter day of start date, remember february has 28 days: \n"))#if it's February in a regular year, we'd expect an input a number between 1 and 28
-    elif first_month in [9,4,6,11]:
-        first_day = int(input("Please enter day of start date. should be between 1 and 30: \n"))# September, April, June and November have 30 days
-    else:
-        first_day = int(input("Please enter day of start date. should be between 1 and 31: \n"))    
-
-    first_time = input("Please enter time of start date in 24 hour format, separated by commas. For example 12hours 30 minutes as 12,30: \n")
-    mylist = first_time.split(',')
-    first_hour = int(mylist[0])
-    first_minute = int(mylist[1])
-
-    # algorithm to obtain end date and time
-    second_year = int(input("Please enter year of the end date: \n"))
-
-    second_month = int(input("Please enter month of the end date figures from 1-12: \n"))
-
-    second_day = int(input("Please enter day of end date: \n"))
-
-    second_time = input("Please enter time of end date in 24 hour format, separated by commas. For example 12hours 30 minutes as 12,30: \n")
-    mylist1 = second_time.split(',')
-    second_hour = int(mylist1[0])
-    second_minute = int(mylist1[1])
-
-
-# Algorithm to find time difference and hours worked
-first_date = datetime(first_year,first_month,first_day,first_hour,first_minute)
-
-second_date = datetime(second_year,second_month,second_day,second_hour,second_minute)
-
-difference_between_datetimes = second_date - first_date
-hours_calculated = difference_between_datetimes.total_seconds()/3600
-total_amount_received = hours_calculated * PayRate
-currency = "$"
-print('Great work %s! You spent %f minutes, which is %f %s on the task. And you earned %s%f' % (username,hours_calculated*60,hours_calculated,'hours',currency,total_amount_received))
-print('Your bill is $200')
-time.sleep(5)
-print("Don't mind me, I'm just kidding.\nBye %s, hope to see you soon!" %username)
-
