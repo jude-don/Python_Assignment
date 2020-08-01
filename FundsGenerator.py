a9924847798823eb7868a485783ba117d0438146
Jude Donkor <judedonkor75@gmail.com>
2020-08-01 09:36:31 +0000

    Update FundsGenerator.py

--- FundsGenerator.py
+++ FundsGenerator.py
@@ -1,11 +1,10 @@
 from datetime import datetime  
-
+import xlsxwriter
 paid = 5  # signifies 5 dollars per hour
-
 # algorithm to obtain start date and time
 task_name = input("Please enter the name of the task you undergoing: \n")
 answer = input("Do you want to use system time? Please choose Y/N. \n")
-if answer == Y:
+if answer == "Y":
     dt = datetime.now()
     first_year = dt.year
     first_month = dt.month
@@ -15,7 +14,7 @@ if answer == Y:
 else:  
     first_year = input("Please enter year of the start date: \n")
     first_year = int(first_year)
-    first_month = input("Please enter month of the start date figures from 1-12: \n")
+    first_month = input("Please enter month of the start date in figures from 1-12: \n")
     first_month = int(first_month)
     first_day = input("Please enter day of start date: \n")
     first_day = int(first_day)
@@ -27,7 +26,7 @@ else:
 # algorithm to obtain end date and time
 second_year = input("Please enter year of the end date: \n")
 second_year = int(second_year)
-second_month = input("Please enter month of the end date figures from 1-12: \n")
+second_month = input("Please enter month of the end date in figures from 1-12: \n")
 second_month = int(second_month)
 second_day = input("Please enter day of end date: \n")
 second_day = int(second_day)
@@ -47,3 +46,31 @@ currency = "$"
 print('He spent %f %s on a task' % (hours_calculated,'hours'))
 print("\n")
 print('He will receive %s %f' % (currency,total_amount_received))
+
+# Converting to an Excel file
+workbook = xlsxwriter.Workbook('TaskHistory.xlsx') 
+worksheet = workbook.add_worksheet() 
+  
+#Setting Headers
+worksheet.write('A1', 'TaskName') 
+worksheet.write('B1', 'StartDate') 
+worksheet.write('C1', 'StartTime') 
+worksheet.write('D1', 'EndDate') 
+worksheet.write('E1', 'EndTime') 
+worksheet.write('F1', 'HoursSpent') 
+worksheet.write('G1', 'AmountReceived') 
+
+
+#Pushing date
+worksheet.write('A2', task_name)
+g = str(first_year)+"-"+str(first_month)+"-"+str(first_day)
+worksheet.write('B2',g )  
+h = str(first_hour)+":"+str(first_minute)
+worksheet.write('C2', h) 
+g1 = str(second_year)+"-"+str(second_month)+"-"+str(second_day)
+worksheet.write('D2',g1 )  
+h1 = str(second_hour)+":"+str(second_minute)
+worksheet.write('E2', h1) 
+worksheet.write('F2', str(hours_calculated)) 
+worksheet.write('G2', str(total_amount_received)) 
+workbook.close()
